e52c531194ab7aa6dfa192064a845d0a27ab0ffd
Sneha-yadav0481 <sneha.25005058@jainuniversity.ac.in>
2026-09-02 08:26:39 +0530

    Add files via upload

---
+++ Ex3.py
@@ -0,0 +1,14 @@
+s="Hello Python"
+print("Original String:",s)
+print("Length:",len(s))
+print("Uppercase:",s.upper())
+print("Lower:",s.lower())
+print("Character at index 6:",s[6])
+print("Position of python:",s.find("python"))
+print("Slice:",s[6:])
+print("Replace:",s.replace("Python","World"))
+print(s)
+print("contain Python:","python" in s)
+print("Concatenation:",s + " Programming")
+s2="Hello  World"
+print("Trim:",s2.strip())
